1ccf93dbffad4c4a2387b6ceb27ba14cbca05a0f
山川拓也 <yamataku@keilab-macbookair2015a.local>
2016-07-11 16:48:53 +0900

    infoのcontrollerを追加、その他微調整

---
+++ information.swift
@@ -0,0 +1,18 @@
+//
+//  information.swift
+//  DB
+//
+//  Created by 山川拓也 on 2016/07/11.
+//  Copyright © 2016年 project03A. All rights reserved.
+//
+
+import UIKit
+
+class information: UIViewController {
+
+    
+    @IBAction func backbutton(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
