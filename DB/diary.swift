1e70eb5ecf65f869a9ebe98f897bddfd3897fcd5
yosei1127 <b1017165@fun.ac.jp>
2017-11-13 15:44:35 +0900

    NavigationBarの追加

--- DB/diary.swift
+++ DB/diary.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class diary: UIViewController{
     
+    let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBOutlet weak var navigationbar: UINavigationBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let navBarImage = UIImage(named: "bar6.png") as UIImage?
+        self.navigationbar.setBackgroundImage(navBarImage, for:. default)
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "haikei.png")!)
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backbutton(_ sender: AnyObject) {
+        appDelegate.pic_segmented = 0
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
+    }
+
 }
